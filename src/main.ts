221a277d7f656256ba7d0c53e5f786bf17c3f091
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2022-04-25 16:48:34 +0900

    style: :sparkles:  vuetifyを導入しました。

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {store,key} from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 import router from './router'
-import { store, key } from '@/store'
-import './index.css'
 
-const app = createApp(App)
-app.use(store, key)
-app.use(router)
-app.mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(store, key)
+  .use(vuetify)
+  .use(router)
+  .mount('#app')
